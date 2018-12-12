5a7ac1f6ca9e279ad919e31bfba307fb4042ec87
Chenjie <345025497@qq.com>
2018-12-12 20:37:38 +0800

    第十一篇：Spring Boot之Scheduling Tasks定时任务

---
+++ springboot-scheduled/src/main/java/com/chenjie/springboot/learn/scheduled/ScheduledTasks.java
@@ -0,0 +1,21 @@
+package com.chenjie.springboot.learn.scheduled;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@Component
+public class ScheduledTasks {
+    private static final Logger log = LoggerFactory.getLogger(ScheduledTasks.class);
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+    @Scheduled(fixedRate = 2000)
+    public void reportCurrentTime() {
+        log.info("The time is : " + dateFormat.format(new Date()));
+    }
+}
